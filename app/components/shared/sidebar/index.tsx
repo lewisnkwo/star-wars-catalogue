0b25ed020c89e1736e48182f237a1abea839234a
Lewis Nkwo <lewisnkwo@Lewiss-MBP.cust.communityfibre.co.uk>
2023-04-20 09:50:51 +0100

    Add sidebar

---
+++ app/components/shared/sidebar/index.tsx
@@ -0,0 +1,15 @@
+const Sidebar = () => {
+  return (
+    <div>
+      <div className="Sidebar__top">
+        <span className="Sidebar__title">Star Wars Catalogue</span>
+        <span className="Sidebar__description">
+          Search your favourite Star Wars characters and find out their info
+        </span>
+      </div>
+      {/* Add sidebar items here */}
+    </div>
+  );
+};
+
+export default Sidebar;
