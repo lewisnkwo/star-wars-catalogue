e020b3238d9d435b1c6e80bbd0ad6003439fddc2
Lewis Nkwo <lewisnkwo@Lewiss-MBP.cust.communityfibre.co.uk>
2023-04-21 11:17:22 +0100

    Add Home page

---
+++ app/components/pages/content/home/index.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState, useRef } from "react";
+import SidebarDetail from "../../../shared/sidebar-detail";
+
+const Home = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const [selectedCharacter, _setSelectedCharacter] = useState<{} | undefined>(
+    undefined
+  );
+
+  const sidebarPostRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth <= 768);
+  }, []);
+
+  useEffect(() => {
+    if (sidebarPostRef?.current) {
+      sidebarPostRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest",
+      });
+    }
+  }, [selectedCharacter]);
+
+  return (
+    <div className={`Home${selectedCharacter ? "-with-sidebar" : ""}`}>
+      <main>
+        <div>
+          <section>
+            <span className="Home__heading">Latest characters</span>
+            <span className="Home__description">
+              Click on a character to view info on their planets
+            </span>
+          </section>
+          <section className="Home__character-list"></section>
+        </div>
+      </main>
+      {selectedCharacter && (
+        <div ref={isMobile ? sidebarPostRef : null}>
+          <SidebarDetail title="" subtitle="" items={[]} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
