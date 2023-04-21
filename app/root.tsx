a4fe39e2a2dd425214cdf0b3039806cb5a933a82
Lewis Nkwo <lewisnkwo@Lewiss-MBP.cust.communityfibre.co.uk>
2023-04-21 15:19:07 +0100

    Add root style paths & add home content styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -6,6 +7,39 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import globalStyles from "./styles/global.css";
+import layoutStyles from "./components/pages/layout/index.css";
+import headerStyles from "./components/shared/header/index.css";
+import homeStyles from "./components/pages/content/home/index.css";
+import characterCardStyles from "./components/pages/content/home/character-card/index.css";
+import footerStyles from "./components/shared/footer/index.css";
+import imageStyles from "./components/shared/image/index.css";
+import sidebarStyles from "./components/shared/sidebar/index.css";
+import sidebarItemStyles from "./components/shared/sidebar/sidebar-item/index.css";
+import sidebarDetailStyles from "./components/shared/sidebar-detail/index.css";
+import sidebarDetailPostItemStyles from "./components/shared/sidebar-detail/post-item/index.css";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Lewis Nkwo",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: globalStyles },
+    { rel: "stylesheet", href: layoutStyles },
+    { rel: "stylesheet", href: headerStyles },
+    { rel: "stylesheet", href: homeStyles },
+    { rel: "stylesheet", href: characterCardStyles },
+    { rel: "stylesheet", href: footerStyles },
+    { rel: "stylesheet", href: imageStyles },
+    { rel: "stylesheet", href: sidebarStyles },
+    { rel: "stylesheet", href: sidebarItemStyles },
+    { rel: "stylesheet", href: sidebarDetailStyles },
+    { rel: "stylesheet", href: sidebarDetailPostItemStyles },
+  ];
+};
 
 export default function App() {
   return (
