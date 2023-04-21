029a44ee8426bf79ef70da9c8fdb0c3aaf69291f
Lewis Nkwo <lewisnkwo@Lewiss-MBP.cust.communityfibre.co.uk>
2023-04-21 12:51:23 +0100

    Add footer styles

--- app/components/shared/footer/index.tsx
+++ app/components/shared/footer/index.tsx
@@ -1,6 +1,11 @@
 const Footer = () => (
   <footer>
-    <span>Star Wars Catalogue by Lewis Nkwo</span>
+    <span>
+      Star Wars Catalogue. Created by Lewis Nkwo.{" "}
+      <a href="https://swapi.dev" target="_blank" rel="noopener noreferrer">
+        Powered by SWAPI
+      </a>
+    </span>
   </footer>
 );
 
