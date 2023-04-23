988dc819e212ce4d51d3fcca7353ee6e4d53dbd5
Lewis Nkwo <lewisnkwo@Lewiss-MBP.cust.communityfibre.co.uk>
2023-04-23 19:15:50 +0100

    Add filter by gender option to Filter Bar

--- app/components/shared/filter-bar/index.tsx
+++ app/components/shared/filter-bar/index.tsx
@@ -25,13 +25,29 @@ const FilterBar = ({ items, isMenuOpen, onMenuClose }: Props) => {
         <div className="FilterBar__top">
           <span className="FilterBar__title">Filter Results</span>
           <span className="FilterBar__description">
-            Filter and sort the results
+            Sort and filter the results
           </span>
         </div>
-        <div className="FilterBar__items" aria-label="Filters">
-          {items.map((item, i) => (
-            <Item key={i} {...item} />
-          ))}
+        <div className="">
+          <section className="FilterBar__items" aria-label="Sort by">
+            <label>Sort</label>
+            {items.map((item, i) => (
+              <Item key={i} {...item} />
+            ))}
+            <label>Filter by Gender</label>
+            <select
+              className="FilterBar__select"
+              name="gender"
+              defaultValue="all"
+              onChange={(e) => e.currentTarget.value}
+              aria-label="Filter by Gender"
+            >
+              <option value="all">All</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="na">N/A</option>
+            </select>
+          </section>
         </div>
       </div>
       {isMenuOpen && (
